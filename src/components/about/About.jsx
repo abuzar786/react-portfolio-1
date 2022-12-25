6146901fa1be47b111905e5b5ab3319467fbbe8b
abuzar786 <49241793+abuzar786@users.noreply.github.com>
2022-12-26 00:09:25 +0100

    About section done
    I completed the about section with responsiveness as well.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <img src={ME} alt="About Image" />
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience </h5>
+              <small>1 Year work</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients </h5>
+              <small>50+ Clients Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects </h5>
+              <small>60+ Completed Projects</small>
+            </article>
+          </div>
+
+          <p>
+            I am experinced software developer and I can develope good websites
+            for your business for your personal portfolio and also I can develop
+            desktop applications.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
