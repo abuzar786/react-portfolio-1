434d8059f4c289e8b4168d8c2e9db4c523a2b624
abuzar786 <49241793+abuzar786@users.noreply.github.com>
2022-12-27 13:48:05 +0100

    Services component finished
    added the service component with its design.

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,146 @@
 import React from "react";
 import "./services.css";
+import { BiCheck } from "react-icons/bi";
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>WordPress Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* End of the WordPress service  */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>XR and Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* End of the XR and Web Developement service  */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Graphic and 3D Designing</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>User interface design with UX</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* End of the Graphic Design service  */}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
