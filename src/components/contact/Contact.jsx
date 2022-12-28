c368b9d9e15c390ad7c9e75edf3c9f284f90bfc4
abuzar786 <49241793+abuzar786@users.noreply.github.com>
2022-12-28 02:49:37 +0100

    contact component finished
    added contact form, styled it and make it responsive

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./contact.css";
 import { MdOutlineMail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
 const Contact = () => {
   return (
     <section id="contact">
@@ -10,30 +12,56 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineMail />
+            <MdOutlineMail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>abuzarhanjra786@gmail.com</h5>
-            <a href="mailto:abuzarhanjra786@gmail.com">Send a Message</a>
+            <a href="mailto:abuzarhanjra786@gmail.com" target="_blank">
+              Send a Message
+            </a>
           </article>
 
           <article className="contact__option">
-            <MdOutlineMail />
+            <BsMessenger className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>abuzarhanjra786@gmail.com</h5>
-            <a href="https://m.me/muhammadabuzar">Send a Message</a>
+            <a href="https://m.me/muhammad.abuzar.9216" target="_blank">
+              Send a Message
+            </a>
           </article>
 
           <article className="contact__option">
-            <MdOutlineMail />
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+4740599866</h5>
-            <a href="mailto:abuzarhanjra786@gmail.com">Send a Message</a>
+            <a
+              href="https://api.whatsapp.com/send?phone=004740599866"
+              target="_blank"
+            >
+              Send a Message
+            </a>
           </article>
         </div>
 
         {/* End of contact options */}
 
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Please write your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
