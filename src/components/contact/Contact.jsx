b4a15c0e6d93f172428aaa316f2fc9ac91d10988
abuzar786 <49241793+abuzar786@users.noreply.github.com>
2022-12-28 01:51:32 +0100

    Finished testimonial component
    constructed the testimonial components and styled it by CSS and make it responsive

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineMail } from "react-icons/md";
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>abuzarhanjra786@gmail.com</h5>
+            <a href="mailto:abuzarhanjra786@gmail.com">Send a Message</a>
+          </article>
+
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>Messenger</h4>
+            <h5>abuzarhanjra786@gmail.com</h5>
+            <a href="https://m.me/muhammadabuzar">Send a Message</a>
+          </article>
+
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>WhatsApp</h4>
+            <h5>+4740599866</h5>
+            <a href="mailto:abuzarhanjra786@gmail.com">Send a Message</a>
+          </article>
+        </div>
+
+        {/* End of contact options */}
+
+        <form action=""></form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/testimonial/Testimonials.jsx
+++ src/components/testimonial/Testimonials.jsx
@@ -4,62 +4,65 @@ import AVTR1 from "../../assets/avatar1.jpg";
 import AVTR2 from "../../assets/avatar2.jpg";
 import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
+
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "tine snow",
+    review: "abc the kind and strong person and recommended",
+  },
+  {
+    avatar: AVTR2,
+    name: "Edward El Hayak",
+    review: "abc the kind and strong person and recommended",
+  },
+  {
+    avatar: AVTR3,
+    name: "Philip Rein",
+    review: "abc the kind and strong person and recommended",
+  },
+  {
+    avatar: AVTR4,
+    name: "Kevin Tan",
+    review: "abc the kind and strong person and recommended",
+  },
+];
+
 const Testimonials = () => {
   return (
     <section id="testimonials">
       <h5>Rreviews from Clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-            
-          </div>
-          <h5>Enest Achiever</h5>
-            <small className="client__review">
-              He is very knowledgeable and expert person and he is very
-              responsive. He did great Job for me and I really want to work with
-              him again and again. He is strongly recommended.
-            </small>
-        </article>
-
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5>Enest Achiever</h5>
-          <small className="client__review">
-            He is very knowledgeable and expert person and he is very
-            responsive. He did great Job for me and I really want to work with
-            him again and again. He is strongly recommended.
-          </small>
-        </article>
-
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5>Enest Achiever</h5>
-          <small className="client__review">
-            He is very knowledgeable and expert person and he is very
-            responsive. He did great Job for me and I really want to work with
-            him again and again. He is strongly recommended.
-          </small>
-        </article>
-
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5>Enest Achiever</h5>
-          <small className="client__review">
-            He is very knowledgeable and expert person and he is very
-            responsive. He did great Job for me and I really want to work with
-            him again and again. He is strongly recommended.
-          </small>
-        </article>
-      </div>
+      <Swiper
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="Avatar One" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
