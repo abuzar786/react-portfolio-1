207f86d2d0410c9a1420a15135e8e737aeaa8bb5
abuzar786 <49241793+abuzar786@users.noreply.github.com>
2022-12-30 03:25:22 +0100

    Footer section done
    constructed footer section and styled it and made it responsive using media queries

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookSquare } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaTwitterSquare } from "react-icons/fa";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Abuzar Techie
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfoli">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">testimonial</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookSquare />
+        </a>
+        <a href="https://instagram.com">
+          <AiFillInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <FaTwitterSquare />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small> &copy; Abuzar Techie. All right reserved  </small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
